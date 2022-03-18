838e5f57df4021a5b83fd68b44b1c34663b2044d
haha_harshit <bkharshitbk@gmail.com>
2022-03-18 17:33:01 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
-import NavBar from './Navbar/NavBar';
+import NavBar from './components/Navbar/NavBar';
+// import NavBar from './Navbar/NavBar';
 // import Btn from './Btn';
 // const name = 'Harshit';
 function App() {
   return (
     <>
     <div className="inp-form">
-        <NavBar/>
+      <NavBar />
     </div>
     </>
   );
