afa30a668f80c0a5a26d3bb0c59ca9f6032e436e
Harshit Kumar <bkharshitbk@gmail.com>
2022-05-26 02:28:27 +0530

    set the app for deployment using gh pages

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ export default function App() {
 
         </Switch>
       </div>
-    </Router>
+      </Router> 
     </>
   );
 }
--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {
     Link
   } from "react-router-dom";
-// const hu="hi"
+
 import PropTypes from 'prop-types'
 
 export default function NavBar(props) {
@@ -19,7 +19,7 @@ export default function NavBar(props) {
       <>
         <nav className={`navbar navbar-expand-lg + ${darkClass}`} style={{backgroundColor:"#a2b9bc", position: "sticky", top: 0, width: '100%', zIndex:2}}>
         <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" To="#">
             <b>{props.title}</b>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,10 +28,10 @@ export default function NavBar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link active" aria-current="page" To="#">Home</Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" To="/about">About</Link>
             </li>
             </ul>
             <form className="d-flex">
